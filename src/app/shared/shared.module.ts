44ffa1f4e94d27704f29f7024d20f972af3cdf5f
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2022-09-23 17:03:13 +0530

    preview page and login data change

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,21 @@ import { CommonHeaderComponent } from './common-header/common-header.component';
 import { ModalBoxComponent } from './modal-box/modal-box.component';
 import { MaterialModule } from '../material.module';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+import { SharedKycProfileViewComponent } from './shared-kyc-profile-view/shared-kyc-profile-view.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     NzSelectModule
+    
   ],
-  declarations: [CommonHeaderComponent,ModalBoxComponent],
+  declarations: [CommonHeaderComponent,ModalBoxComponent,SharedKycProfileViewComponent],
   exports:[
     CommonHeaderComponent,
     ModalBoxComponent,
-    NzSelectModule
+    NzSelectModule,
+    SharedKycProfileViewComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 
