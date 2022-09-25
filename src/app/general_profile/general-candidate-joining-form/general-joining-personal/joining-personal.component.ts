1149b71d1f446a323b204a3f6c68f2705d91d8e5
vivek <vivekkumar-r@lntecc.com>
2022-09-25 16:23:42 +0530

    changes madein form
    changes madein form

--- src/app/general_profile/general-candidate-joining-form/general-joining-personal/joining-personal.component.ts
+++ src/app/general_profile/general-candidate-joining-form/general-joining-personal/joining-personal.component.ts
@@ -52,16 +52,28 @@ export class GeneralJoiningPersonalComponent implements OnInit, AfterViewInit, O
 
   marital_list = [
     {
+      name: 'Single',
+      value: 'Single'
+    },
+    {
       name: 'Married',
       value: 'Married'
     },
     {
-      name: 'Unmarried',
-      value: 'Unmarried'
-    },
-    {
       name: 'Widow',
       value: 'Widow'
+    },
+    {
+      name: 'Divorced',
+      value: 'Divorced'
+    },
+    {
+      name: 'Separated',
+      value: 'Separated'
+    },
+    {
+      name: 'Others',
+      value: 'Others'
     }
   ];
   no_children_list = ['0', '1', '2', '3', '4', '5'];
@@ -121,6 +133,10 @@ export class GeneralJoiningPersonalComponent implements OnInit, AfterViewInit, O
     {
       label: 'Female',
       value: 'Female'
+    },
+    {
+      label: 'Others ',
+      value: 'Others '
     }
   ]
   // Form control name declaration Start
