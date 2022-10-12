c8de777282c1ca2bfaa6e00394c5cbdc5880ce30
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2022-10-12 17:09:53 +0530

    forgot password page UI rewamp

--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/internal/operators/filter';
 import { AppConfigService } from '../../config/app-config.service';
-
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -11,9 +12,19 @@ export class SignupComponent implements OnInit {
   getCurrentYear = this.appConfig.getCurrentYear();
   isloginPG = true;
   constructor(
-    private appConfig: AppConfigService
+    private appConfig: AppConfigService,
+    private router: Router
   ) {
-
+    router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event:any) =>
+     {
+        if(event.url=='/login'){
+          this.isloginPG = true
+        }else{
+          this.isloginPG = false
+        }
+        console.log(event.url);
+     });
 
   }
 
