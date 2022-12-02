7451b9a4a22f1e60cbc885c842f966ebaf6270d7
Gokul Appaswamy <gokul.appaswamy@lntedutech.com>
2022-12-02 12:30:26 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,8 @@ export const environment = {
   cryptoEncryptionKey:"(!@#Passcode!@#)",
   API_BASE_URL:"https://skilledge.lntedutech.com",
   blobToken:"?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2221-11-23T19:12:41Z&st=2021-11-23T11:12:41Z&spr=https,http&sig=R6%2BlZGrzjuFs1aAy2uUG%2BNkjVig5%2F8disv01i86VK8M%3D",
-  supportEmail:"assess.support@lntedutech.com"
+  supportEmail:"assess.support@lntedutech.com",
+  register_Redirect:"https://skillexchange.lntedutech.com/register",
+  SKILLEX_BASE_URL:"https://skilledge.lntedutech.com",
+  MASTER_BASE_URL:"https://campus.lntedutech.com/dcampus"
 };
